0f96cf8422ff96e2026b1592d1fb2def13f4f750
noueii <74404028+noueii@users.noreply.github.com>
2024-04-07 00:31:50 +0300

    1.0

--- next-app/src/components/app/CookieConsent.jsx
+++ next-app/src/components/app/CookieConsent.jsx
@@ -128,7 +128,7 @@ function CookieConsent({store}) {
               <div className='flex gap-2 flex-col overflow-scroll'>
                 {consents.map((x) =>{
                   return (
-                  <div className='flex flex-col gap-2'>
+                  <div className='flex flex-col gap-2' key={x.title}>
                     <div className='flex gap-4 justify-between flex-wrap'>
                       <p className='text-xl '>{x.title}</p>
                       <div className='flex gap-2 items-center'>
