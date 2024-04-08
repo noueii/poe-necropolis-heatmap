b65c164c0238b3d64d6b6e3d52eebe6b78d4494d
noueii <74404028+noueii@users.noreply.github.com>
2024-04-08 03:47:43 +0300

    meybe fix

--- next-app/src/lib/cookie.js
+++ next-app/src/lib/cookie.js
@@ -1,14 +1,14 @@
 "use server"
 import { cookies } from 'next/headers'
-const cookieStore = cookies()
+
 
 
 export async function setCCUCookies(value){
-  cookieStore.set('ccu', JSON.stringify(value))
+  cookies().set('ccu', JSON.stringify(value))
 }
 
 export async function getTimeCookie(){
-  return cookies().get('lf')
+  return await cookies().get('lf')
 }
 export async function setTimeCookies(value){
   cookies().set('lf', JSON.stringify(value))
